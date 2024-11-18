0c3c5a60284a2766cfe9e11f57009ef478d1d3e0
MhmdNat <mia67@mail.aub.edu>
2024-11-18 14:19:40 +0200

    updated code, everything automated, has bugs

--- Scraping.py
+++ Scraping.py
@@ -10,13 +10,15 @@ import csv
 import os
 
 
-PATH = "C:\\Users\\user 2\\Desktop\\WebScraping Stuff\\chromedriver.exe"
+PATH = "C:\\Users\\user 2\\Desktop\\WebScraping Stuff\\chromedriver.exe" # CHANGE FOR YOUR SPECIFIC DIRECTORY
 service = Service(PATH)
 wd = webdriver.Chrome(service=service)
 
 
-def get_urls_from_google(wd, delay, max_images):
+def get_urls_from_google(wd, delay, url, left):
     '''Transforms images to a set of image_urls using a page url'''
+    wd.get(url)
+    
     
     
     def scroll_down(wd):
@@ -24,42 +26,32 @@ def get_urls_from_google(wd, delay, max_images):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);") #execute javascript script
         time.sleep(delay) # wait for images to load
         
-        
-    url = "https://www.inaturalist.org/observations?quality_grade=research"
-    wd.get(url) # open url page
-    
+    thumbnails = []
     image_urls = set() #not list to make sure we have each url once only
-    i=1 # currently counter of images, need to change it to thubmnail counter
-    stop = False
-    while i <max_images and stop==False:
+    while True:
         scroll_down(wd)
-        
         thumbnails = wd.find_elements(By.CSS_SELECTOR, ".photo.has-photo")  # returns a list of all thumbnails, at most 96 thumbnails
         
-        for img in thumbnails[len(image_urls) : max_images]:
-            if stop: break
+        for img in thumbnails[len(image_urls) : ]:
             try:
                 img.click() # click thumbnail to show bigger image
+                print("thumbnail")
                 try:
                     time.sleep(delay) # wait for image to load
                     image_elements = wd.find_elements(By.CSS_SELECTOR, '.image-gallery-image img') # list of all images inside the thumbnail
                     for image in image_elements:
-                        if i>=max_images:
-                            stop = True
-                            break
+
                         if image.get_attribute('src') and 'http' in image.get_attribute('src'): # make sure image has a http link
                             image_urls.add(image.get_attribute('src')) #get url andadd it to our set
                             print("Image added!")
-                            i+=1 
                 finally:
                     wd.back() # wd returns to main page
                 
                 
             except:
                 continue
-                
-
-    return image_urls # return set of image urls
+        if len(thumbnails)==96 or len(thumbnails) == left: # stop when all the page has been read
+            return image_urls
             
         
 def download_image(download_path, url, file_name):
@@ -95,15 +87,16 @@ def get_id(wd, name): #name of target plant
     href = href.split('taxa/')
     plant_id = href[1] #extract the id
     
-    '''url format becomes https://www.inaturalist.org/observations?quality_grade=research&taxon_id={plant_id}'''
+    # url format becomes https://www.inaturalist.org/observations?quality_grade=research&taxon_id={plant_id}
     return plant_id
 
 
 def read_csv_plants():
     '''read names from csv file into a list '''
+    # place name of csv in directory
     with open('C:\\Users\\user 2\\Desktop\\WebScraping Stuff\\Species name.csv', mode = 'r', newline='', encoding = 'utf-8') as file: #open csv file
         csv_reader = csv.reader(file) # create a reader
-        species_names = [row[0] for row in csv_reader] # read first column in each row
+        species_names = [row[0] for row in csv_reader if row] # read first column in each row
     return species_names # return a list of all species in csv
 
 
@@ -111,7 +104,7 @@ def species_name_to_id():
     '''this is for transferrring the names to ids'''
     name_id_list = []
     species = read_csv_plants() # list of specie names read from file
-    
+     
     for name in species:
         plant_id = get_id(wd,name) # for each plant get the id
         name_id_list.append((name,plant_id)) #tuple
@@ -124,14 +117,39 @@ def new_path(new_path):
         os.makedirs(new_path)
     return new_path
 
+
+def get_number_of_observations(wd):
+    time.sleep(10)
+    element = wd.find_element(By.CLASS_NAME, 'stat-value')
+    number_text = element.text
+    print(number_text)
+    number = int(number_text.replace(",", ""))
+    return number
+
         
 def main():
-    plant_name_ids = species_name_to_id() #returns tuple (name, id)
+    plant_name_ids = species_name_to_id() #returns list of tuples (name, id)
     
     for name, ID in plant_name_ids: #for the tuple returned by species_name_to_id
-    
-        page_url = f"https://www.inaturalist.org/observations?quality_grade=research&taxon_id={ID}" # open page using id
-        urls = get_urls_from_google(wd, 4, 3, page_url) # get image urls
+        page_url = f"https://www.inaturalist.org/observations?quality_grade=research&taxon_id={ID}"
+        wd.get(page_url)
+        time.sleep(3)
+        total_thumbnails = get_number_of_observations(wd)
+        number_of_full_pages = total_thumbnails // 96 # 96 thumbnails per page
+        print("num_full:",number_of_full_pages)
+        thumbnails_left = total_thumbnails % 96
+        print("left: ",thumbnails_left)
+        
+        urls = set()
+        for i in range(1,number_of_full_pages+1):
+            urls = set()
+            page_url = f"https://www.inaturalist.org/observations?page={i}quality_grade=research&taxon_id={ID}" # open page using id and page number
+            urls.update(get_urls_from_google(wd, 3, page_url, left = -1)) # get image urls and add them to our url set
+            
+        if thumbnails_left:
+            page_url = f"https://www.inaturalist.org/observations?page={number_of_full_pages+1}quality_grade=research&taxon_id={ID}"
+            urls.update(get_urls_from_google(wd, 3, page_url, left = thumbnails_left))
+
         
         for i, url in enumerate(urls):
             path = new_path(f"C:\\Users\\user 2\\Desktop\\WebScraping Stuff\\Imgs\\{name}/") #inserts images into a folder with the name of plant
